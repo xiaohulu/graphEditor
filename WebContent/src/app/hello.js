79559cd64da5d8ad3e94897d35f71e74babd9b2c
xiaohulu <idocument@qq.com>
2016-08-14 09:08:35 +0800

    first commit

---
+++ WebContent/src/app/hello.js
@@ -0,0 +1,11 @@
+/*
+ * A very simple AMD module with no dependencies
+ */
+define([], function () {
+    return {
+        greet: function (name) {
+            name = name || 'world';
+            return 'Hello, ' + name + '!';
+        }
+    };
+});
---
+++ WebContent/tests/unit/hello.js
@@ -0,0 +1,14 @@
+define(function (require) {
+    var registerSuite = require('intern!object');
+    var assert = require('intern/chai!assert');
+    var hello = require('src/app/hello');
+    registerSuite({
+        name: 'hello',
+        greet: function () {
+            assert.strictEqual(hello.greet('Murray'), 'Hello, Murray!',
+                'hello.greet should return a greeting for the person named in the first argument');
+            assert.strictEqual(hello.greet(), 'Hello, world!',
+                'hello.greet with no arguments should return a greeting to "world"');
+        }
+    });
+});
